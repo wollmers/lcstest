790445f9c8bd47564453d7901480ab9b8a355fa9
Helmut Wollmersdorfer <helmut@wollmersdorfer.at>
2019-11-18 18:18:36 +0100

    unbelievable fast without static

--- lcstest.c
+++ lcstest.c
@@ -15,35 +15,34 @@
 
 static const uint64_t width = 64;
 
-int count_bits_fast(uint64_t bits) {
-    // 12 operations
-    bits = bits - ((bits >> 1) & 0x5555555555555555ull);
-    bits = (bits & 0x3333333333333333ull) + ((bits >> 2) & 0x3333333333333333ull);
-    // (bytesof(bits) -1) * bitsofbyte = (8-1)*8 = 56 -------------------------------vv
-    return ((bits + (bits >> 4) & 0x0f0f0f0f0f0f0f0full) * 0x0101010101010101ull) >> 56;
+int count_bits(uint64_t bits) {
+  bits = bits - ((bits >> 1) & 0x5555555555555555ull);
+  bits = (bits & 0x3333333333333333ull) + ((bits >> 2) & 0x3333333333333333ull);
+  // (bytesof(bits) -1) * bitsofbyte = (8-1)*8 = 56 -------------------------------vv
+  return ((bits + (bits >> 4) & 0x0f0f0f0f0f0f0f0full) * 0x0101010101010101ull) >> 56;
 }
 
-
 int llcs_asci (char * a, char * b, uint32_t alen, uint32_t blen) {
+    // static uint64_t posbits[128] = { 0 }; //    73.4 (M/sec)
+    // uint64_t posbits[128] = { 0 };        // 53050.4 (M/sec)
+    uint64_t posbits[128];                   // 56338.0 (M/sec)
 
-    static uint64_t posbits[128] = { 0 };
-    uint64_t i;
-    
-    // for (i=0; i < 128; i++) { posbits[i] = 0; } // needed
+    uint64_t i;   
+    for (i=0; i < 128; i++) { posbits[i] = 0; } // needed
 
     for (i=0; i < alen; i++) {
       	posbits[(unsigned int)a[i]] |= 0x1ull << (i % width);
     }    
 
     uint64_t v = ~0ull;
-
+    
     for (i=0; i < blen; i++) {
       uint64_t p = posbits[(unsigned int)b[i]];
       uint64_t u = v & p;
       v = (v + u) | (v - u);
     }
 
-    return count_bits_fast(~v);  // portable
+    return count_bits(~v);  // portable
     //return _mm_popcnt_u64(~v); 
 }
 
@@ -58,7 +57,7 @@ int main (void) {
     uint32_t iters     = 1000000;
     uint32_t megaiters = 1;
 
-    // m=10, n=11, llcs=7, sim=0.667
+    // m=10, n=11, llcs=7, d=4, sim=0.667
     char str1[] = "Choerephon";
     char str2[] = "Chrerrplzon";
     
